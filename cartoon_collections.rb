6a497acdedf8f55714d04594c9cf2fc0103de94d
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-09 14:43:32 -0600

    commit tree 6e9fa20c48c3f101c2b83eec4c1bb0aa74368e8f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+def find_the_cheese(ingredients)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |item|
+    return item if cheeses.include?(item)
+  end
+ end
+ 
